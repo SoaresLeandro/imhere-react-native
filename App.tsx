8d9a3622f5d3ac7345f771f95a27b8eb2acbf508
Leandro <lss.91@live.com>
2025-03-10 09:24:19 -0400

    Aplicando estilização

--- App.tsx
+++ App.tsx
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+export default function App(){
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.eventName}>React Native</Text>
+      <Button title='Click-me'></Button>
+
+      <Button title='Dont click-me'></Button>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+      flex: 1,
+      backgroundColor: '#140101',
+      padding: 24
+    },
+    eventName:{
+      color: '#FDFCFE',
+      fontSize: 24,
+      fontWeight: 'bold',
+      marginTop: 48
+    },
+    buttonOne: {
+      backgroundColor: 'green'
+    },
+    buttonTwo:{
+      backgroundColor: 'blue'
+    }
 });
